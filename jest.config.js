834c92a76c992fcdffb9b07e2e6b65ec55601d68
SanaSanat <sst7212@gmail.com>
2024-04-18 21:37:43 -0700

    advance.yml

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  // testEnvironmentOptions:{
+  //   url: "localhost:8001/api/v1",
+  // }
+
+"reporters": [ 
+  "default", ["jest-junit",{outputDirectory:"reports"}],
+  ['jest-html-reporters',{
+    publicPath:"reports"
+  }]
+ ]
 };
